9262634903f92eece11c28806e89114cb2579816
Raquelmc95 <raquelmc95@outlook.com>
2025-04-08 23:57:29 +0200

    Eliminar el proyecto Proyecto_final del repositorio hmtl

--- Proyecto_final/src/Ejercicio08JDBC/Main.java
+++
@@ -1,8 +0,0 @@
-package Ejercicio08JDBC;
-
-public class Main {
-    public static void main(String[] args) {
-        Oficina oficina = new Oficina(1,"Malaga", "300", 2020);
-        OficinaDAO.create(oficina);
-    }
-}
--- Proyecto_final/src/Ejercicio11JDBC/Main.java
+++
@@ -1,7 +0,0 @@
-package Ejercicio11JDBC;
-
-public class Main {
-    public static void main(String[] args) {
-        OficinaDAO.read();
-    }
-}
